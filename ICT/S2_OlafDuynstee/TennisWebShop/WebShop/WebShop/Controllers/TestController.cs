c4077dea272df169f3919d7391ca0d3b1578b51b
duynstee <467073@student.fontys.nl>
2021-09-17 13:25:08 +0200

    1,

---
+++ ICT/S2_OlafDuynstee/TennisWebShop/WebShop/WebShop/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace WebShop.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        // Require using System.Text.Encodings.Web;
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello  " + name;
+            ViewData["NumTimes"] = numTimes;
+            
+            return View();
+        }
+    }
+}
---
+++ ICT/S2_OlafDuynstee/TennisWebShop/WebShop/WebShop/Models/ProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.Models
+{
+    public class ProductViewModel
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
