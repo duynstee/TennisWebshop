2a142be01b9ce95c3b08d7bb0679a3ae5c2fbc9c
duynstee <467073@student.fontys.nl>
2021-10-08 19:02:31 +0200

    Update Laptop

---
+++ ICT/S2_OlafDuynstee/TennisWebShop/SQLFiles/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ProductID] INT NOT NULL PRIMARY KEY, 
+    [ProductName] VARCHAR(50) NOT NULL, 
+    [Price] INT NULL, 
+    [Quantity] INT NULL
+)
---
+++ ICT/S2_OlafDuynstee/TennisWebShop/WebShop/Database.Data/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Database.Data
+{
+    public class ProductDatabaseManager
+    {
+        private string connectionString =
+            "Data Source=localhost;Initial Catalog=WebShopDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public List<ProductDto> GetAllProducts()
+        {
+            List<ProductDto> products = new List<ProductDto>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand query = new SqlCommand("select * from Product"))
+                {
+                    conn.Open();
+
+                    var reader = query.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        ProductDto prod = new ProductDto();
+                        prod.ProductID = reader.GetInt32(0);
+                        prod.ProductName = reader.GetString(1);
+                        prod.Price = reader.GetInt32(2);
+                        prod.Quantity = reader.GetInt32(3);
+
+                        products.Add(prod);
+                    }
+                }
+            }
+
+            return products;
+        }
+    }
+}
