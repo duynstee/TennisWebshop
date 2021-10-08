01b63481afbbb296e7131f4e0703cadde4299087
duynstee <74192436+duynstee@users.noreply.github.com>
2021-10-09 00:02:03 +0200

    pc update

---
+++ ICT/S2_OlafDuynstee/TennisWebShop/WebShop/Logic/Admin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class Admin
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+
+
+        private Product AddProduct()
+        {
+
+        }
+
+        private Product RemoveProduct(Product product)
+        {
+
+        }
+    }
+}
---
+++ ICT/S2_OlafDuynstee/TennisWebShop/WebShop/Logic/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+
+
+        public Product()
+        {
+
+        }
+        public Product(string name, string size, int quantity, string description)
+        {
+            name = this.Name;
+            size = this.Size;
+            quantity = this.Quantity;
+            description = this.Description;
+        }
+    }
+}
