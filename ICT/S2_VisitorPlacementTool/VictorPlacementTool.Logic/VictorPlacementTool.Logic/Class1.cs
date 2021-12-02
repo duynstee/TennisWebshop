3d1f8ed4b9a18ee0c8bb585ff6c3a9a47caa7e06
duynstee <74192436+duynstee@users.noreply.github.com>
2021-12-02 10:07:30 +0100

    aanmaken classes in logaritme opdracht

---
+++ ICT/S2_VisitorPlacementTool/VictorPlacementTool.Logic/VictorPlacementTool.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VictorPlacementTool.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ ICT/S2_VisitorPlacementTool/VictorPlacementTool.Logic/VictorPlacementTool.Logic/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VictorPlacementTool.Logic
+{
+    class Event
+    {
+        public string Name;
+        public DateTime Date;
+
+        public Event(string name, DateTime date)
+        {
+            this.Name = name;
+            this.Date = date;
+        }
+    }
+}
